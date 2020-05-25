3a22ce00caa591cad8445560d35ab03d6ebcb856
George Lee <glee@chowly.com>
2020-05-24 21:29:25 -0500

    fix hill shadow

--- scripts/color.js
+++ scripts/color.js
@@ -2,56 +2,15 @@
 // color //
 // ================================== //
 
-// function randomColorValues() {
-//   var randH = Math.random();
-//   var randS = Math.random() * 0.1 + 0.5;
-//   var randL = Math.random() * 0.1 + 0.4; 
-//   return { h: randH, s: randS, l: randL };
-// }
-
-// function getHue(color) {
-//   return color.h % 1.0;
-// }
-
-// function primaryColor(color) {
-//   return "hsl(" + getHue(color) * 360 + ", " + (color.s % 1.0) * 100 + "%, " + (color.l % 1.0) * 100 + "%)";
-// }
-
-// function secondaryColor(color) {
-//   return "hsl(" + (getHue(color) * 360 + 210)+ ", " + (color.s % 1.0) * 100 + "%, " + (color.l % 1.0) * 100 + "%)";
-// }
-
-// function hillColor() {
-//   // console.log(randomColor)
-//   var baseColor = randomColorValues();
-//   console.log(baseColor);
-//   return  {
-//     h: baseColor.h + Math.random() * 0.2 - 0.2, 
-//     s: baseColor.s + Math.random() * 0.2 - 0.20,
-//     l: baseColor.l + Math.random() * 0.1 + 0.2
-//   }
-// }
-
-// function skyColor() {
-  
-// }
-
 function hillShadow(color) {
-  // shadowColor = {
-  //   h: color.h,
-  //   s: color.s,
-  //   l: color.l - .3
-  // }
-
   ctx.shadowOffsetX = 10;
   ctx.shadowColor = color;
   ctx.shadowBlur = 30;
 }
 
-
 function complementaryPositions(startingPosition) {
   var complementaryPosition = (startingPosition + 180) % 360;
-  return [startingPosition, complementaryPosition]
+  return [startingPosition, complementaryPosition, startingPosition]
 }
 
 function analagousPositions(startingPosition) {
@@ -72,12 +31,10 @@ function triadicPositions(startingPosition) {
   return [startingPosition, complementaryPosition, thirdPosition]
 }
 
-
 function generateHslaPoints () {
-
   let startingPosition = Math.floor(Math.random() * 360);
-  let positionsArray = [complementaryPositions, analagousPositions, splitPositions, triadicPositions];
-  let positions = positionsArray[Math.floor(Math.random() * positionsArray.length)](startingPosition);
+  let positionNames = [complementaryPositions, analagousPositions, splitPositions, triadicPositions];
+  let positions = positionNames[Math.floor(Math.random() * positionNames.length)](startingPosition);
 
   return positions
 }
